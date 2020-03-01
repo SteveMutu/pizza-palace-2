142bab63f05a0e4c172a6e3e7d7c1e798e3afdf3
SteveMutu <mutusteve0@gmail.com>
2020-03-01 12:43:19 +0300

    functions on order

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var size , crust ,toppings ;
+var total= 0;
+function order(size,crust ,toppings ,total){
+    this.size = size;
+    this.crust = crust;
+    this.toppings =toppings;
+    this.total = total;
+}
 $(document).ready(function(){
-    
+    $("button.order").click(function(event){
+        
+    })
+
 })
