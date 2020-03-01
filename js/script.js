ed9479c01028d5ffcd698ceaa3ccb289b4dd67b4
SteveMutu <mutusteve0@gmail.com>
2020-03-01 14:32:07 +0300

    add functions

--- js/script.js
+++ js/script.js
@@ -8,7 +8,35 @@ function order(size,crust ,toppings ,total){
 }
 $(document).ready(function(){
     $("button.order").click(function(event){
-        
+        var size=$("#size option:selected").val();
+        var crust=$("#crust option:selected").val();
+        var toppings=("#toppings option:selected").val();
     })
-
+if ("large"){
+    price = 1500;
+}    
+if("medium"){
+    price = 900;
+}    
+if("small"){
+    price = 650;
+}    
+if("crispy"){
+    price = 100
+}
+if("stuffed"){
+    price =100
+}
+if("gluten-free"){
+    price = 100
+}
+if ("bacon"){
+    price = 50
+}
+if ("chicken"){
+    prize = 50
+}
+if ("babycon"){
+    price = 50
+}
 })
